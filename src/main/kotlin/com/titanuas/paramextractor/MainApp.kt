3982c2f06d977f33c0d461ac3ab7c19a83e59427
Mohammad Adib <m.a.adib96@gmail.com>
2025-11-24 23:48:28 -0800

    Logo and branding

--- src/main/kotlin/com/titanuas/paramextractor/MainApp.kt
+++ src/main/kotlin/com/titanuas/paramextractor/MainApp.kt
@@ -6,11 +6,12 @@ import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.stage.Stage
+import javafx.scene.image.Image
 
 class MainApp : Application() {
 
     override fun start(primaryStage: Stage) {
-        primaryStage.title = "Ardupilot Parameter Extractor"
+        primaryStage.title = "Ardupilot Parameter Extractor - By Titan Dynamics"
 
         // Load FXML
         val fxmlLoader = FXMLLoader(javaClass.getResource("/com/titanuas/paramextractor/MainView.fxml"))
@@ -26,6 +27,16 @@ class MainApp : Application() {
         val cssResource = javaClass.getResource("/com/titanuas/paramextractor/dark.css")
         if (cssResource != null) {
             scene.stylesheets.add(cssResource.toExternalForm())
+        }
+
+        // Set window icon if available
+        val iconResource = javaClass.getResource("/com/titanuas/paramextractor/icon.png")
+        if (iconResource != null) {
+            try {
+                primaryStage.icons.add(Image(iconResource.toExternalForm()))
+            } catch (_: Exception) {
+                // ignore if icon cannot be loaded
+            }
         }
 
         primaryStage.scene = scene
